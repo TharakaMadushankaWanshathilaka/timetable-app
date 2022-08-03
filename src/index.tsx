b72091d1503b88201a2fe33b646f8d733a507216
LassazVegaz <lasindunuwanga@gmail.com>
2022-08-03 18:11:46 +0530

    styles moved

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
+import "./styles/index.scss";
+import "./styles/about.scss";
+import "./styles/animations.scss";
+import "./styles/style.scss";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import { App } from "./App";
 
 const root = ReactDOM.createRoot(
